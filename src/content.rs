9f680e14135e6bb743a6bff5f376ad325ed9ce31
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-10-19 09:16:14 -0700

    Commited to new JSON format by moving away from 'new-' configuration. Beautified the JSON.

--- src/content.rs
+++ src/content.rs
@@ -4,7 +4,7 @@ use crate::kart::*;
 
 pub fn load_content(mut players: HashMap<String, f32>) -> HashMap<String, f32> {
     // https://stackoverflow.com/q/63657897
-    let path = "./new-content.json";
+    let path = "./content.json";
     let data = fs::read_to_string(path).expect("Unable to read file");
     let res: Vec<HashMap<String, f32>> =
         serde_json::from_str(&data).expect("Unable to parse");
